76cf1af3745fb332bdb04973440df31d8396f99c
Arnaud Bonnet <2a.bonnet@gmail.com>
2025-12-28 00:24:48 +0100

    feat: installation SymfonyCasts ResetPasswordBundle
    - Ajout de symfonycasts/reset-password-bundle v1.24.0
    - Configuration du bundle avec repository personnalisé

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
